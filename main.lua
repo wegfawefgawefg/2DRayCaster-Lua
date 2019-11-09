d8aa7fb4f5fbaa22754569e39a5e8ad3fdeff9cd
vegalious <668es218pur@gmail.com>
2019-11-09 17:03:27 -0600

    made sum libs

--- main.lua
+++ main.lua
@@ -1,43 +1,26 @@
-function newShape(x, y, numPoints, radius, rotAngle)
-    local shape = {}
-    shape.x = x
-    shape.y = y
-    shape.radius = radius
-    shape.angle = rotAngle
-    shape.verts = {}
+local Vec2 = require("vec2")
+local CPolygon = require("cpolygon")
 
-    -- fill verts
-    local anglePerSegment = 2 * math.pi / (numPoints - 1)
-    for angle=0, 2*math.pi, anglePerSegment do
-        shape.verts[#shape.verts+1] = math.cos(angle) * radius
-        shape.verts[#shape.verts+1] = math.sin(angle) * radius
-    end
-    for i,k in pairs(shape.verts) do
-        print(k)
-    end
-    return shape
-end
-
-function makePolygonVerts(numPoints, radius)
-    verts = {}
-    local anglePerSegment = 2 * math.pi / (numPoints)
-    for angle=0, 2*math.pi, anglePerSegment do
-        verts[#verts+1] = math.cos(angle) * radius
-        verts[#verts+1] = math.sin(angle) * radius
-    end
-    return verts
+function addAnotherWallPolygon()
+    local wall = {}
+    wall.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()/2, "dynamic")
+    local numVerts = 4--love.math.random(3, 7)
+    local radius = 5--love.math.random(10, 20)
+    wall.shape = love.physics.newPolygonShape(CPolygon.makePolygonVerts(numVerts, radius))
+    wall.fixture = love.physics.newFixture(wall.body, wall.shape, 1)
+    objects.walls[#objects.walls+1] = wall
 end
 
 function love.load()
     --  globals
-    camera = newShape(
+    camera = CPolygon.newShape(
         love.graphics.getWidth()/2,
         love.graphics.getHeight()/2,
         3, 10, 0
     )
     
     numRays = 50
-    fov = 120
+    fov = 40
 
     --  create physics world
     love.physics.setMeter(64) --the height of a meter our worlds will be 64px
@@ -46,18 +29,14 @@ function love.load()
     --  create some physics bodies
     objects = {}
     objects.walls = {}
-    for i=0,10 do
-        local wall = {}
-        wall.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()/2, "dynamic")
-        wall.shape = love.physics.newPolygonShape(makePolygonVerts(love.math.random(3, 7), love.math.random(10, 50)))
-        wall.fixture = love.physics.newFixture(wall.body, wall.shape, 1)
-        objects.walls[#objects.walls+1] = wall
+    for i=0,100 do
+        addAnotherWallPolygon()
     end
 
     --create camera physics body
     objects.camera = {}
     objects.camera.body = love.physics.newBody(world, love.graphics.getWidth()/4, love.graphics.getHeight()/4, "dynamic")
-    objects.camera.shape = love.physics.newPolygonShape(makePolygonVerts(3, 10))
+    objects.camera.shape = love.physics.newPolygonShape(CPolygon.makePolygonVerts(3, 10))
     objects.camera.fixture = love.physics.newFixture(objects.camera.body, objects.camera.shape, 1)
 end
 
@@ -93,6 +72,9 @@ function checkKeypresses( dt )
             objects.camera.body:getX() + ldx,
             objects.camera.body:getY() + ldy    )
     end
+    if love.keyboard.isDown("space") then
+        addAnotherWallPolygon()
+    end
 end
 
 function love.update(dt)
@@ -115,19 +97,22 @@ end
 function love.draw()
     drawWalls()
     drawCamera()
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+    love.graphics.print("Num Bodies: "..tostring(#objects.walls), 10, 30)
 
     -- --  draw rays coming out of camera
-    -- local anglePerRay = fov / numRays
-    -- local halfFov = fov / 2
-    -- local startingAngle = camera.angle - halfFov
-    -- local endingAngle = camera.angle + halfFov
-    -- for angle=startingAngle, endingAngle, anglePerRay do
-    --     --  draw line centered on camera, of long length
-    --     love.graphics.push()
-    --     love.graphics.rotate(angle)
-    --     love.graphics.translate(camera.x, camera.y)
-    --     love.graphics.line(0, 0)
-    -- end
+    local anglePerRay = math.rad(fov / numRays)
+    local halfFov = math.rad(fov / 2)
+    local startingAngle = objects.camera.body:getAngle() - halfFov - math.pi/2
+    local endingAngle = objects.camera.body:getAngle() + halfFov - math.pi/2
+    for angle=startingAngle, endingAngle, anglePerRay do
+        --  draw line centered on camera, of long length
+        love.graphics.push()
+        love.graphics.translate(objects.camera.body:getX(), objects.camera.body:getY())
+        love.graphics.rotate(angle)
+        love.graphics.line(0, 0, 0, 100)
+        love.graphics.pop()
+    end
 end
 
 function love.keypressed(key, scancode, isrepeat)
